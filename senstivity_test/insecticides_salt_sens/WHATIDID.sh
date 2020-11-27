2e1a121a441e5abf6d4c7f3d6778e076d143fe54
gmercer <UCFAGA0@ucl.ac.uk>
2020-11-27 17:18:10 +0000

    correcting the "data has to be distributed for linear regression" fallacy. Model residuals not distribution of dependent variable

--- senstivity_test/insecticides_salt_sens/WHATIDID.sh
+++ senstivity_test/insecticides_salt_sens/WHATIDID.sh
@@ -9,10 +9,10 @@ tail -n +14 input/insecticide_salt_sens.csv \
 cut -d ',' -f 3- tmp/rem_sett_format > tmp/rem_well_allo
 
 # sort and remove blank incorporated duplicates
-# seq -s ',' 0 5 960 > tmp/time
+# seq -s ',' 0 5 955 > tmp/time
 # head tmp/time | tr ',' '\n' | awk 'END{print NR}'
-# output is 193, add 1 for rowname = 194
-sort tmp/rem_well_allo | cut -d ',' -f -193 > tmp/rem_blank_dupli
+# output is 192, add 1 for rowname = 193
+sort tmp/rem_well_allo | cut -d ',' -f 1,194-385 > tmp/rem_blank_dupli
 
 # get time in minute format and save in results
 seq -s ',' 0 5 955 > tmp/time
@@ -20,6 +20,8 @@ echo "time" > tmp/time_header
 paste -d ',' tmp/time_header tmp/time > tmp/time_line
 cat tmp/rem_blank_dupli >> tmp/time_line
 
-cp tmp/time_line tmp/insecticide_salt_sens_formatted.csv
+grep -v -E 'blank' tmp/time_line > tmp/time_line_1
+
+cp tmp/time_line_1 tmp/insecticide_salt_sens_formatted.csv
 
 cp tmp/insecticide_salt_sens_formatted.csv results/insecticide_salt_sens_formatted.csv
