6779696403806f731d5ec1570cecc2e2da384f5b
gmercer <UCFAGA0@ucl.ac.uk>
2020-12-04 17:24:05 +0000

    salt interaction analysis started

--- senstivity_test/insecticides_salt_sens/WHATIDID.sh
+++ senstivity_test/insecticides_salt_sens/WHATIDID.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env zsh
 
+function format_growth_assay_data(){
 
 # remove machine settings information weird gaps
-tail -n +14 input/insecticide_salt_sens.csv \
+tail -n +14 input/$1.csv \
 | tr '\t' ',' > tmp/rem_sett_format
 
 # remove well designation and concatenate columns 1 and 2 with a -
@@ -12,7 +13,7 @@ cut -d ',' -f 3- tmp/rem_sett_format > tmp/rem_well_allo
 # seq -s ',' 0 5 955 > tmp/time
 # head tmp/time | tr ',' '\n' | awk 'END{print NR}'
 # output is 192, add 1 for rowname = 193
-sort tmp/rem_well_allo | cut -d ',' -f 1,194-385 > tmp/rem_blank_dupli
+sort tmp/rem_well_allo | cut -d ',' -f 1,195-387 > tmp/rem_blank_dupli
 
 # get time in minute format and save in results
 seq -s ',' 0 5 955 > tmp/time
@@ -22,6 +23,22 @@ cat tmp/rem_blank_dupli >> tmp/time_line
 
 grep -v -E 'blank' tmp/time_line > tmp/time_line_1
 
-cp tmp/time_line_1 tmp/insecticide_salt_sens_formatted.csv
+cp tmp/time_line_1 tmp/$2_formatted.csv
 
-cp tmp/insecticide_salt_sens_formatted.csv results/insecticide_salt_sens_formatted.csv
+cp tmp/$2_formatted.csv results/$2_formatted.csv
+
+}
+
+seq -s ',' 0 5 960 > tmp/time
+echo "time" > tmp/time_header
+paste -d ',' tmp/time_header tmp/time > results/formatted_compiled.csv
+
+VAR=(`seq -s ' ' 1 1 2`)
+
+for i in $VAR; do
+ 
+format_growth_assay_data salt-interaction-${i} replicate_${i}
+
+tail -n +2 tmp/replicate_${i}_formatted.csv >> results/formatted_compiled.csv
+
+done
